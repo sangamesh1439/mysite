fb205d4f8024ef47bc91db223129ae64b2044f8d
Sangamesh Somawar <sansomaw@in.ibm.com>
2018-05-28 14:41:16 +0530

    some fixes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,11 +5,11 @@ import homeImg from '../../public/media/home.jpg'
 export default withSiteData(() => (
   <div>    
     <div id="shop-name">
-        <div className="jumbotron">        
-          <img id='home-image'src={homeImg} alt=''></img>
+      <img id='home-image'src={homeImg} alt=''></img>            
+        <div className="jumbotron">                  
           <h1> Somawar Computers</h1>
           <p>one stop solution for all your computer needs</p>
         </div>        
-      </div>           
+      </div>                 
   </div>
 ))
